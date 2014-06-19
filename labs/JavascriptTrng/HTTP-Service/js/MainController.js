c9592a5d18df8457f81dc2ee5fb028e206c4ae05
Ariel Barrera <ariel.barrera@intel.com>
2014-06-18 18:01:23 -0700

    Routing Service added

--- labs/JavascriptTrng/HTTP-Service/js/MainController.js
+++ labs/JavascriptTrng/HTTP-Service/js/MainController.js
@@ -3,24 +3,9 @@
  */
 (function() {
 
-    var app = angular.module("githubViewer", []);
+    var app = angular.module("githubViewer");
 
-    var MainController = function($scope, github, $interval, $log, $anchorScroll, $location) {
-
-        var onUserComplete = function(data) {
-            $scope.user = data;
-            github.getRepos($scope.user).then(onRepos, onError);
-        };
-
-        var onRepos = function(data){
-            $scope.repos = data;
-            $location.hash("userDetails");
-            $anchorScroll();
-        };
-
-        var onError = function(reason) {
-            $scope.error = "Could not fetch the user";
-        };
+    var MainController = function($scope, $interval, $location) {
 
         var decrementCountdown = function(){
             $scope.countdown -= 1;
@@ -35,22 +20,17 @@
         };
 
         $scope.search = function(username){
-            $log.info("Searching for " + username);
-            github.getUser(username).then(onUserComplete, onError);
             if(countdownInterval){
                 $interval.cancel(countdownInterval);
                 $scope.countdown = null;
             }
+            $location.path("/user/" + username);
         };
 
 
         $scope.username = "angular";
-        $scope.message = "GitHub Viewer";
-        $scope.repoSortOrder = "-stargazers_count";
         $scope.countdown = 5;
         startCountdown();
-
-
     };
 
     app.controller("MainController", MainController);
